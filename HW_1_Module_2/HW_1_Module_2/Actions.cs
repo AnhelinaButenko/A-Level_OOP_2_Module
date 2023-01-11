b1d55eb145f5e8904553f7c38b6c9f1cea868fcb
Angelina Butenko <angelinaa2712@gmail.com>
2023-01-11 21:11:06 +0200

    Revert "Delete HW_1_Module_2 directory"
    This reverts commit bebf9ba408130a0435a36cb76825d4c816d6fba0.

---
+++ HW_1_Module_2/HW_1_Module_2/Actions.cs
@@ -0,0 +1,34 @@
+﻿using Common;
+namespace HW_1_Module_2
+{
+    static class Actions
+    {
+        private static FileLogger _logger;
+
+        static Actions()
+        {
+            _logger = FileLogger.Instance;
+        }
+
+        public static Result InfoAction() 
+        {
+            _logger.LogInfo($"Start method: {nameof(InfoAction)}");
+            Result result = new Result() { Status = LogType.Info};
+            return result;
+        }
+
+        public static Result WarningAction()
+        {
+            _logger.LogWarning($"Skipped logic in method: {nameof(WarningAction)}");
+            Result result = new Result() { Status = LogType.Warning};
+            return result;
+        }
+
+        public static Result ErrorAction()
+        {
+            _logger.LogError($"Skipped logic in method: {nameof(ErrorAction)}");
+            Result result = new Result() {Status = LogType.Error ,ErrorMessage = "Broke a logic" };
+            return result;
+        }
+    }
+}
---
+++ HW_1_Module_2/HW_1_Module_2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HW_1_Module_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Starter.Run();
+        }
+    }
+}
